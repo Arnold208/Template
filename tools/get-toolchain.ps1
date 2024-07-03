30d65b241ce931ae32f89bc3b8119b7f9743d160
Arnold Sylvian Kimkpe <140562304+Arnold208@users.noreply.github.com>
2024-07-03 22:50:39 +0000

    Update get-toolchain.ps1

--- tools/get-toolchain.ps1
+++ tools/get-toolchain.ps1
@@ -3,9 +3,9 @@
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 
-echo "`nChecking for installed packages..."
+Write-Host "`nChecking for installed packages..."
 
-# Paths and file names
+# Paths, file names, and hash values for verification
 $gccarm_path = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/'
 $gccarm_file = 'gcc-arm-none-eabi-10.3-2021.10-win32.exe'
 $gccarm_name = 'GNU Arm Embedded Toolchain'
@@ -21,78 +21,52 @@ $termite_file = 'termite-3.4.exe'
 $termite_name = 'Termite v3.4'
 $termite_hash = 'CA440B6C7F6EAA812BA5F8BF42AED86E02022CA50A1C72585168C9B671D0FE19'
 
-# Function to check if a program is installed
-function Is-ProgramInstalled($programName) {
-    $installed = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" -ErrorAction SilentlyContinue | 
-                 Where-Object { $_.DisplayName -like "*$programName*" }
-    return $installed -ne $null
-}
-
-# Function to check if a directory exists
-function Check-Directory {
-    param (
-        [string]$directoryPath
-    )
-
-    if (Test-Path $directoryPath -PathType Container) {
-        Write-Output "The directory $directoryPath exists."
-        return $true
-    } else {
-        Write-Output "The directory $directoryPath does not exist."
-        return $false
-    }
-}
-
-$gccarm_installed = Check-Directory "C:\Program Files (x86)\GNU Arm Embedded Toolchain"
+# Check installations
+$gccarm_installed = Test-Path "C:\Program Files (x86)\GNU Arm Embedded Toolchain"
 $cmake_installed = Get-Command cmake -ErrorAction SilentlyContinue
-$termite_installed = Check-Directory "C:\Program Files (x86)\Termite"
+$termite_installed = Test-Path "C:\Program Files (x86)\Termite"
 
 if ($gccarm_installed -and $cmake_installed -and $termite_installed) {
-    echo "All required packages are already installed."
+    Write-Host "All required packages are already installed."
     exit 0
 }
 
-echo "`nDownloading packages..."
+Write-Host "`nDownloading packages..."
 
 $wc = New-Object System.Net.WebClient
 $wc.Headers['User-Agent'] = "Mozilla/4.0"
 
+# Download and install GCC-ARM if not installed
 if (-not $gccarm_installed) {
-    echo "(1/3) $gccarm_name"
-    if (-not (Test-Path "$env:TEMP\$gccarm_file") -Or ((Get-FileHash "$env:TEMP\$gccarm_file").Hash -ne $gccarm_hash)) {
-        $wc.DownloadFile("$gccarm_path\$gccarm_file", "$env:TEMP\$gccarm_file")
+    $localPath = "$env:TEMP\$gccarm_file"
+    Write-Host "(1/3) Downloading $gccarm_name..."
+    if (-not (Test-Path $localPath) -or ((Get-FileHash $localPath).Hash -ne $gccarm_hash)) {
+        $wc.DownloadFile("$gccarm_path$gccarm_file", $localPath)
+        Write-Host "Installing $gccarm_name..."
+        Start-Process -FilePath $localPath -ArgumentList "/S" -Wait
     }
 }
 
+# Download and install CMake if not installed
 if (-not $cmake_installed) {
-    echo "(2/3) $cmake_name"
-    if (-not (Test-Path "$env:TEMP\$cmake_file") -Or ((Get-FileHash "$env:TEMP\$cmake_file").Hash -ne $cmake_hash)) {
-        $wc.DownloadFile("$cmake_path\$cmake_file", "$env:TEMP\$cmake_file")
+    $localPath = "$env:TEMP\$cmake_file"
+    Write-Host "(2/3) Downloading $cmake_name..."
+    if (-not (Test-Path $localPath) -or ((Get-FileHash $localPath).Hash -ne $cmake_hash)) {
+        $wc.DownloadFile("$cmake_path$cmake_file", $localPath)
+        Write-Host "Installing $cmake_name..."
+        Start-Process -FilePath $localPath -ArgumentList "ADD_CMAKE_TO_PATH=System /passive" -Wait
     }
 }
 
+# Download and install Termite if not installed
 if (-not $termite_installed) {
-    echo "(3/3) $termite_name"
-    if (-not (Test-Path "$env:TEMP\$termite_file") -Or ((Get-FileHash "$env:TEMP\$termite_file").Hash -ne $termite_hash)) {
-        $wc.DownloadFile("$termite_path\$termite_file", "$env:TEMP\$termite_file")
+    $localPath = "$env:TEMP\$termite_file"
+    Write-Host "(3/3) Downloading $termite_name..."
+    if (-not (Test-Path $localPath) -or ((Get-FileHash $localPath).Hash -ne $termite_hash)) {
+        $wc.DownloadFile("$termite_path$termite_file", $localPath)
+        Write-Host "Installing $termite_name..."
+        Start-Process -FilePath $localPath -ArgumentList "/S" -Wait
     }
 }
 
-echo "`nInstalling packages..."
-
-if (-not $gccarm_installed) {
-    echo "(1/3) $gccarm_name"
-    Start-Process -FilePath "$env:TEMP\$gccarm_file" -ArgumentList "/S /P /R" -Wait
-}
-
-if (-not $cmake_installed) {
-    echo "(2/3) $cmake_name"
-    Start-Process -FilePath "$env:TEMP\$cmake_file" -ArgumentList "ADD_CMAKE_TO_PATH=System /passive" -Wait
-}
-
-if (-not $termite_installed) {
-    echo "(3/3) $termite_name"
-    Start-Process -FilePath "$env:TEMP\$termite_file" -ArgumentList "/S" -Wait
-}
-
-echo "`nInstallation complete!"
+Write-Host "`nInstallation complete!"
